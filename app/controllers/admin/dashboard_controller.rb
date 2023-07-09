2511470857390c7c35a72b9730ffb3aff127d0e6
ConlanSchool <conlan.school@gmail.com>
2023-07-09 12:27:09 -0700

    Add basic auth for admin page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
-class Admin::DashboardController < ApplicationController
-  def show
+class Admin::BaseController < ApplicationController
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.configuration.http_authenticate[0] && password == Rails.configuration.http_authenticate[1]
+    end
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.http_authenticate = [ENV["BASIC_AUTH_NAME"], ENV["BASIC_AUTH_PASSWORD"]]
+
 end
