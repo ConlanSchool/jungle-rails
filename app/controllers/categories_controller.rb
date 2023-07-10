50b209a0c4805931c1a3eaf4cc3e76e435337ff5
ConlanSchool <conlan.school@gmail.com>
2023-07-10 14:10:55 -0700

    Add admin categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,29 @@
-class CategoriesController < ApplicationController
+module Admin
+  class CategoriesController < ApplicationController
+    http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD']
 
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+    def index
+      @categories = Category.all
+    end
+
+    def new
+      @category = Category.new
+    end
+
+    def create
+      @category = Category.new(category_params)
+
+      if @category.save
+        redirect_to admin_categories_path, notice: 'Category was successfully created.'
+      else
+        render :new
+      end
+    end
+
+    private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
   end
-
 end
